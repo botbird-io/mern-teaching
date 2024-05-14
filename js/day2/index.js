9348999a3359b35b13f08306f244e54d198f6b5f
sanket-sapate <sanketsapatevnit@gmail.com>
2024-05-14 18:38:40 +0530

    Variable declaration, data types

---
+++ js/day2/index.js
@@ -0,0 +1,55 @@
+// var name1 = 'John'
+// let name2 = 'Doe'
+// const name3 = 'Jane'
+
+// console.log(name1,name2,name3)
+
+// // This code will reassign the value of name1, name2, and name3
+// name1 = 'Jane'
+// name2 = 'Doe1'
+// name3 = 'John'
+
+// console.log(name1,name2,name3)
+
+// Data type
+
+// // Primitive data type
+// // 1. String
+// var name1 = 'John Doe'
+// console.log(typeof name1,name1)
+// // 2. Number
+// var age = 30
+// console.log(typeof age, age) 
+// // 3. Boolean
+// var isMarried = true
+// console.log(typeof isMarried, isMarried)
+// // 4. Undefined
+// var address
+// console.log(typeof address, address)
+// // 5. Null
+// var city = null
+// console.log(typeof city, city)
+
+// // Non-primitive data type
+// // 1. Object
+// var person = {}
+// var person = {
+//     name: 'John Doe',
+//     age: 30,
+//     isMarried: true,
+//     address: null
+// }
+// console.log(typeof person, person.age, person.name1)
+// person.age = 31
+// console.log(person.age)
+// // 2. Array
+// var fruits = ['Apple', 'Banana', 'Mango']
+// console.log(typeof fruits,fruits, fruits[0])
+// fruits[0] = 'Orange'
+// console.log(fruits[0])
+// 3. Function
+function sayHello(parameter1) {
+    console.log('Hello',parameter1)
+}
+sayHello(1)
+sayHello()

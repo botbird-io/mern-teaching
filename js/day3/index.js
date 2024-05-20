509231d3492a20180941b0898ddc4dc66f9033f5
sanket-sapate <sanketsapatevnit@gmail.com>
2024-05-20 18:58:50 +0530

    if and loops

---
+++ js/day3/index.js
@@ -0,0 +1,32 @@
+let total = 110
+let discount = 0
+
+// if(total>150){
+//     discount = 0.1
+// }else{
+//     discount = 0.05
+// }
+
+// console.log('You have to pay', total - total*discount, 'for the item')
+
+// if(total>150){
+//     discount = 0.1
+// }else if(total>100){
+//     discount = 0.05
+// }else{
+//     discount = 0
+// }
+
+// console.log('You have to pay', total - total*discount, 'for the item')
+
+
+// for(let i=1; i<100000000; i=i*2){
+//     console.log(i)
+// }
+
+let i = 1
+
+while(i<100000000){
+    console.log(i)
+    i = i -1
+}

28e1dc5412ca083da69d3753e0c386a3273175d4
sanket-sapate <sanketsapatevnit@gmail.com>
2024-05-30 19:14:48 +0530

    count characters in string

---
+++ js/day9/index.js
@@ -0,0 +1,10 @@
+let str = 'coding room'
+
+// {c: 1, o: 3, d: 1, i: 1, n: 1, g: 1, r: 1, m: 1}
+
+let obj = {}
+console.log(obj)
+for (let i = 0; i < str.length; i++) {
+    obj[str[i]]++
+}
+console.log(obj)
